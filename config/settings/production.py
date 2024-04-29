7b63cb01a1d5985933ea81ce10e00bdee2d9c16c
earthcomfy <hanagetbel93@gmail.com>
2024-04-29 18:42:58 +0300

    Update: Static url path in prod

--- config/settings/production.py
+++ config/settings/production.py
@@ -23,5 +23,4 @@ AWS_DEFAULT_ACL = "public-read"
 
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-STATIC_URL = "{}/{}/".format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-STATIC_ROOT = "static/"
+STATIC_URL = f"https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/"
