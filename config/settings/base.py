574e2d70de40bc6539b3522d2124dae6dd7a59fd
earthcomfy <hanagetbel93@gmail.com>
2024-04-30 00:31:03 +0300

    New: Add custom password validator

--- config/settings/base.py
+++ config/settings/base.py
@@ -107,12 +107,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 8,
+        },
     },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+    {
+        "NAME": "apps.common.validators.CustomPasswordValidator",
     },
 ]
 
