050a012f645bb9b9ac6438b508040e1211f40f1a
earthcomfy <hanagetbel93@gmail.com>
2024-05-10 16:48:30 +0300

    Update django storages

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,8 +8,6 @@ INSTALLED_APPS += ["storages"]  # noqa: F405
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# Moving static assets to DigitalOcean Spaces as per:
-# https://www.digitalocean.com/community/tutorials/how-to-set-up-object-storage-with-django
 AWS_ACCESS_KEY_ID = config("STATIC_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = config("STATIC_SECRET_KEY")
 
@@ -22,9 +20,14 @@ AWS_LOCATION = "static"
 AWS_DEFAULT_ACL = "public-read"
 
 STORAGES = {
+    "default": {
+        "BACKEND": "apps.common.storage_backends.PublicMediaStorage",
+    },
     "staticfiles": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "BACKEND": "apps.common.storage_backends.StaticStorage",
     },
 }
 
+PUBLIC_MEDIA_LOCATION = "media"
 STATIC_URL = f"https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/"
+MEDIA_URL = f"https://{AWS_S3_ENDPOINT_URL}/{PUBLIC_MEDIA_LOCATION}/"
