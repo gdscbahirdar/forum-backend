17cc699c43fbb12b0830c8170f6f9a10a10d923a
nabroleonx <nabroleonx@gmail.com>
2024-05-12 14:12:10 +0300

    Update: CORS allowed origins configuration for production

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,6 +1,10 @@
 from .base import *  # noqa
+from decouple import Csv, config
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# CORS
+CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", cast=Csv())
