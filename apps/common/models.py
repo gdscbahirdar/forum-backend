3109e5d32a7c6698460b2f105fa3bedfe8c44345
earthcomfy <hanagetbel93@gmail.com>
2024-05-13 09:47:27 +0300

    Add is_active field to models to allow soft-deletion

--- apps/common/models.py
+++ apps/common/models.py
@@ -12,6 +12,7 @@ class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         abstract = True
