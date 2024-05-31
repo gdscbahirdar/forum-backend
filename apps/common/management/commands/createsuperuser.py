2b1e02cdb76ab52e57716da8ee1f682749092038
nabroleonx <nabroleonx@gmail.com>
2024-05-31 15:06:16 +0300

    Update: Override createsuperuser with custom fields

--- apps/common/management/commands/createsuperuser.py
+++ apps/common/management/commands/createsuperuser.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
-
 from apps.rbac.models.role_models import Role, UserRole
 
 User = get_user_model()
@@ -11,9 +10,9 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("--username", type=str, help="The username of the new superuser")
-        parser.add_argument("--first_name", type=str, help="The firstname of the new superuser")
+        parser.add_argument("--first_name", type=str, help="The first name of the new superuser")
         parser.add_argument("--middle_name", type=str, help="The middle name of the new superuser")
-        parser.add_argument("--last_name", type=str, help="The lastname of the new superuser")
+        parser.add_argument("--last_name", type=str, help="The last name of the new superuser")
         parser.add_argument("--password", type=str, help="The password of the new superuser")
 
     def handle(self, *args, **options):
@@ -22,6 +21,27 @@ class Command(BaseCommand):
         middle_name = options["middle_name"]
         last_name = options["last_name"]
         password = options["password"]
+
+        if not username:
+            username = input("Username: ")
+        if not first_name:
+            first_name = input("First name: ")
+        if not middle_name:
+            middle_name = input("Middle name: ")
+        if not last_name:
+            last_name = input("Last name: ")
+        if not password:
+            from django.contrib.auth.password_validation import validate_password
+            from getpass import getpass
+
+            password = getpass("Password: ")
+            password_confirmation = getpass("Password (again): ")
+            if password != password_confirmation:
+                raise CommandError("Passwords do not match.")
+            try:
+                validate_password(password)
+            except Exception as e:
+                raise CommandError(f"Password validation error: {e}")
 
         if User.objects.filter(username=username).exists():
             raise CommandError(f"A user with username '{username}' already exists.")
